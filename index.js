e7af4a482633d5733fce7d3fff8bad562c56b904
Zach Ruiz <powersbooth10@gmail.com>
2022-07-05 13:29:15 -0500

    commit tree 46c323987c011d2f7863df3285bcfa29157d8a74

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const names = ["Charlie", "Samip", "Ali"]
+function writeCards(names, event) {
+    const names2 = [];
+    for (let i = 0; i < names.length; i++) {
+        names2.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return names2;
+}
+
+writeCards()
+
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+
+}
+countDown(number);
